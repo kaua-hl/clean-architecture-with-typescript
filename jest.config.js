b37507c3ddfe1ce90cb2aabad866b8b71e73a34a
Kaua Hl <profissional.kaua@gmail.com>
2022-03-03 19:56:49 -0300

    fix: config roots

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  root: ["<rootDir>/src"],
-  testEnviroment: "node",
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
   moduleNameMapper: {
-    "@/{.*}": "<rootDir>/src/$1",
+    "@/(.*)": "<rootDir>/src/$1",
   },
 };
